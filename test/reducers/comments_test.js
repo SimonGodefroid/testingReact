1e659585c2f914189649147020e9e58c4e1a70df
SimonGodefroid <simon.godefroid@gmail.com>
2017-11-29 14:09:32 +0100

    writting the reducer to pass the test

--- test/reducers/comments_test.js
+++ test/reducers/comments_test.js
@@ -5,7 +5,7 @@ import { SAVE_COMMENT } from '../../src/actions/types';
 describe('Comments Reducer', () => {
 	it('handles action with unknown type', () => {
 		// expect(commentReducer()).to.be.instanceof(Array);
-		expect(commentReducer()).to.eql([]);
+		expect(commentReducer(undefined, {})).to.eql([]);
 	});
 	it('handles action of type SAVE_COMMENT', () => {
 		const action = { type: SAVE_COMMENT, payload: 'new comment' };
